3927b614ef6b5d3a05907ed59db0466496555944
oypservices <41708206+oypservices@users.noreply.github.com>
2019-02-12 20:02:31 -0500

    save

--- Helper.js
+++ Helper.js
@@ -152,50 +152,32 @@ function OYPAPIJSONTransform(template, date) {
 	API Send Mail
 ********************************************************************************************************************/
 
-function OYPAPISendMail(headers, message) {
+function OYPAPISendMail(headers, msgObj) {
 
 	return new Promise ((resolve, reject) => {
 
   			var resource = 'sendmail';
+				var msg = {} ;
 				console.log ('sendmail');
 
+				//format the message
 
-				message = {
-  "to": "brian@oypservices.com",
-  "subject": "Its working....Email sent successfully",
-  "templateId": "d-dbd4fd2a6cbf42c6837e8198ca9564b0",
-  "html": "data",
-  "dynamic_template_data": {
-    "total": "$ 239.85",
-    "items": [
-      {
-        "text": "Nike Sneakers",
-        "image": "https://marketing-image-production.s3.amazonaws.com/uploads/8dda1131320a6d978b515cc04ed479df259a458d5d45d58b6b381cae0bf9588113e80ef912f69e8c4cc1ef1a0297e8eefdb7b270064cc046b79a44e21b811802.png",
-        "price": "$ 79.95"
-      },
-      {
-        "text": "Old Line Sneakers",
-        "image": "https://marketing-image-production.s3.amazonaws.com/uploads/3629f54390ead663d4eb7c53702e492de63299d7c5f7239efdc693b09b9b28c82c924225dcd8dcb65732d5ca7b7b753c5f17e056405bbd4596e4e63a96ae5018.png",
-        "price": "$ 79.95"
-      },
-      {
-        "text": "Blue Line Sneakers",
-        "image": "https://marketing-image-production.s3.amazonaws.com/uploads/00731ed18eff0ad5da890d876c456c3124a4e44cb48196533e9b95fb2b959b7194c2dc7637b788341d1ff4f88d1dc88e23f7e3704726d313c57f350911dd2bd0.png",
-        "price": "$ 79.95"
-      }
-    ],
-    "emailsubject": "New API Gateway Subject",
-    "receipt": true,
-    "name": "Sample Name",
-    "address01": "1234 Fake St.",
-    "address02": "Apt. 123",
-    "city": "Place",
-    "state": "CO",
-    "zip": "80202"
-  }
-}
+				if (msgObj.to != undefined)
+					 msg.to = msgObj.to ;
 
-			  OYPServicesAPIPost( resource, headers, message )
+				if (msgObj.from != undefined)
+	 					 msg.from = msgObj.from ;
+
+				if (msgObj.subject != undefined)
+						 	msg.subject = msgObj.subject ;
+
+				if (msgObj.html != undefined)
+							msg.html = msgObj.html ;
+
+				if (msgObj.dynamic_template_data != undefined)
+							msg.dynamic_template_data = msgObj.dynamic_template_data ;
+
+			  OYPServicesAPIPost( resource, headers, msg )
 					.then (result => { resolve(result) ;})
 					.catch (err => {reject (err); })
 
