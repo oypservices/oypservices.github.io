b8abf862bc1175c2e877129729c5a4ca317e4208
oypservices <41708206+oypservices@users.noreply.github.com>
2019-02-24 20:43:24 -0500

    save

--- IFIContactNote.js
+++ IFIContactNote.js
@@ -349,7 +349,6 @@ function validateContactNote(event, view, data)
 
           var diff =(dt2.getTime() - dt1.getTime()) / 1000;
           diff /= 60;
-          return Math.abs(Math.round(diff));
 
          alert ("Date diff " + diff);
          return false;
