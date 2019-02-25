0780a852df68a047fd53f0022f887078f7a39967
oypservices <41708206+oypservices@users.noreply.github.com>
2019-02-24 22:17:34 -0500

    save

--- IFIContactNote.js
+++ IFIContactNote.js
@@ -365,7 +365,7 @@ function validateContactNote(event, view, data)
           if (diff < 0 )
               msg = "Contact End Date cannot be less than Contact Start Date";
 
-          if (msg = "" && diff < 30)
+          if (msg == "" && diff < 30)
               msg = "Contact End Date cannot be less than Contact Start Date";
 
           if (msg != "") {
