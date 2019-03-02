3e862a6e254d44514db954cbad7fce3ac84d9a6d
oypservices <41708206+oypservices@users.noreply.github.com>
2019-03-02 17:46:25 -0500

    save

--- IFIContactNote.js
+++ IFIContactNote.js
@@ -569,7 +569,7 @@ function checkFinalizeDate(view, data)
     {
       if ( recOverridExpireDate != undefined && recOverridExpireDate != "")
        // recOverride date is a PA exception.  Validate the timefram against this date instead of contact start date
-        targetDate = convertDateTime ( fldOverridExpireDate, "12:00") ;
+        targetDate = convertDateTime ( recOverridExpireDate, "12:00") ;
       else {
         var fldContactDateStart = $(fldPrefix +  dbContactNotes.ContactDateStart).val() ;
         targetDate = convertDateTime ( fldContactDateStart, "12:00") ;
