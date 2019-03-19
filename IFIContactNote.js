329de4991391aaaebc890a82b961fe5a80611d1a
oypservices <41708206+oypservices@users.noreply.github.com>
2019-03-19 19:18:12 -0400

    save

--- IFIContactNote.js
+++ IFIContactNote.js
@@ -212,7 +212,7 @@ function hideShowContactNoteFields(view, val, data) {
       $(fldPrefix +  dbContactNotes.ContactNoteStatus).show();
       $(fldPrefix +  dbContactNotes.ClientIRP).show();
       $(fldPrefix +  dbContactNotes.ContactDateStart).show();
-      $(fldPrefix +  dbContactNotes.ContactDateStart + "-time").hide();
+      $("#" + view.key + "-"  dbContactNotes.ContactDateStart + "-time").hide();
       $(fldPrefix +  dbContactNotes.IRPGoals).show();
 
       $(fldPrefix +  dbContactNotes.ClientResponses).show();
